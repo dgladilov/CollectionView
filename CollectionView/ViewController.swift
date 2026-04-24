db6bb1d628569d3a58916dd5c942eecbb3af891f
Dmitrii Gladilov <d.gladilov005@gnmail.com>
2026-04-24 09:25:48 +0300

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Дмитрий on 24.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
